a25a68205344079be19a7231e707764d4f41ba2a
KiddHermez <dayvidnavarrete@gmail.com>
2023-06-06 16:43:44 -0500

    Fix Not Random Number Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
+'use client';
 import Container from '@/components/Container';
-import { randomInt } from 'crypto';
+import { Random } from 'random-js';
+import { useEffect, useState } from 'react';
 
+const random = new Random();
 export default function Home() {
-    const data = Array.from({ length: 4 }, () => randomInt(101));
-    let result = 0;
-    data.map((num) => (result += num));
+    const [data, setData] = useState<number[]>();
+    const [result, setResult] = useState(0);
+
+    useEffect(() => {
+        const newData = Array.from({ length: 4 }, () => random.integer(0, 100));
+        setData(newData);
+
+        let res = 0;
+        for (const num of newData) {
+            res += num;
+        }
+        setResult(res);
+    }, []);
+
     return (
         <main className='grid h-screen bg-white md:place-content-center'>
-            <Container data={data} result={Math.floor(result / 4)} />
+            <Container
+                data={data ?? [80, 92, 61, 72]}
+                result={Math.floor(result / 4)}
+            />
         </main>
     );
 }
